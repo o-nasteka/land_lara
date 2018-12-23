4102d887ea46b1cb7f54ce50b3bda89635a66fa1
nasteka <oleg.nasteka@gmail.com>
2018-12-23 20:17:06 +0200

    Routers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,68 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',
+        [
+            'as' => 'home',
+            function () {return view('welcome');}
+        ]);
+
+Route::get('/article/{id}',
+    [
+        'as' => 'article',
+        function ($id) {
+        echo $id;
+}
+    ]);
+
+Route::get('/page/{cat}/{id}', function ($id) {
+
+    echo '<pre>';
+//    echo Config::get('app.locale');
+    echo $id;
+    echo '</pre>';
+    // return view('page');
+});
+
+
+/*
+Route::post('/comments', function (){
+
+    echo '<pre>';
+
+    var_dump($_POST);
+
+    echo '</pre>';
+
+});
+*/
+
+Route::match(['get','post'], '/comments', function (){
+
+    echo '<pre>';
+
+    var_dump($_POST);
+
+    echo '</pre>';
+
+});
+
+Route::group(['prefix'=>'admin/{id}'], function (){
+
+    Route::get('page/create/{var}', function ($id){
+
+        $route = Route::current();
+        echo '<pre>';
+//        print_r($route->getName());
+        echo $route->getParameter('var', 12);
+        print_r( $route->parameters() );
+        echo '<pre>';
+
+//        return redirect()->route('article', ['id'=>'25']);
+    })->name('createpage');
+
+    Route::get('page/edit', function (){
+        echo 'page/edit';
+    });
+
 });
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello World!</div>
+                <div class="title">Page!</div>
             </div>
         </div>
     </body>
